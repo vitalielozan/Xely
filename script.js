2267a5e297861e1a89e959a08cac6ca1d5ea4204
Vitascha <vitalielozan17@gmail.com>
2025-03-11 15:39:47 +0100

    Schimbari in fisierul script,js

---
+++ script.js
@@ -0,0 +1,10 @@
+const myArrow = document.querySelector("#myArrow");
+
+myArrow.addEventListener("click", (e) => {
+    /*     if() {
+            myArrow.style.visibility = "hidden";
+        } else {
+            myArrow.style.visibility = "visible";
+        } */
+    console.log(e);
+});

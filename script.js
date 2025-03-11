f782d076b329aa358166b95bdc7e5f04c4656ccd
Vitascha <vitalielozan17@gmail.com>
2025-03-11 22:13:37 +0100

    Schimbari in fisierul script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const myArrow = document.querySelector("#myArrow");
+
 window.addEventListener("scroll", onScroll);
-myArrow.addEventListener("click", () => {
-    myArrow.style.visibility = "hidden";
+
+myArrow.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    console.log("Arrow clicked");
+    myArrow.style.display = "none";
 });
 
 function onScroll() {
     const scrolledTo = window.scrollY + window.innerHeight;
-    const threshold = 3350;
+    const threshold = document.body.scrollHeight * 0.75;
     const isReachBottom = document.body.scrollHeight - threshold <= scrolledTo;
     if (isReachBottom) {
-        myArrow.style.visibility = "visible";
+        myArrow.style.display = "block";
     };
 };

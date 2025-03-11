58427f500f123d8a6c7b042f10a26d70af3d90eb
Vitascha <vitalielozan17@gmail.com>
2025-03-11 20:58:36 +0100

    Lucrari in fisierul script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const myArrow = document.querySelector("#myArrow");
 
-
+window.addEventListener("scroll", onScroll);
 
 myArrow.addEventListener("click", () => {
+
     myArrow.style.visibility = "hidden";
+
 });
 
-const onscroll = () => {
+function onScroll() {
     const scrolledTo = window.scrollY + window.innerHeight;
-    const threshold = 3750;
+    const threshold = 3350;
     const isReachBottom = document.body.scrollHeight - threshold <= scrolledTo;
     if (isReachBottom) {
         myArrow.style.visibility = "visible";
     };
 };
-
-window.addEventListener("scroll", onscroll);
-
-

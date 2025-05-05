ca885c70e260092624760380cf27aa60853b7b36
VitalieLozan <vitalielozan17@outlook.com>
2025-05-05 17:56:02 +0200

    new commit

--- README.md
+++ README.md
@@ -9,4 +9,7 @@
 ## Technology
 
 - HTML
+
 - Bootstrap
+
+- JavaScript
--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const myArrow = document.querySelector("#myArrow");
+const myArrow = document.querySelector('#myArrow');
 let arrowClicked = false;
 
-window.addEventListener("scroll", onScroll);
-myArrow.addEventListener("click", (e) => {
-    e.stopImmediatePropagation();
-    arrowClicked = true;
-    myArrow.style.display = "none";
+window.addEventListener('scroll', onScroll);
+myArrow.addEventListener('click', (e) => {
+  e.stopImmediatePropagation();
+  arrowClicked = true;
+  myArrow.style.display = 'none';
 });
 
 function onScroll() {
-    const scrolledTo = window.scrollY + window.innerHeight;
-    const threshold = document.body.scrollHeight * 0.65;
-    const isReachBottom = document.body.scrollHeight - threshold <= scrolledTo;
-    if (isReachBottom) {
-        myArrow.style.display = "block";
-    } else {
-        myArrow.style.display = "none";
-    }
-};
+  const scrolledTo = window.scrollY + window.innerHeight;
+  const threshold = document.body.scrollHeight * 0.65;
+  const isReachBottom = document.body.scrollHeight - threshold <= scrolledTo;
+  if (isReachBottom) {
+    myArrow.style.display = 'block';
+  } else {
+    myArrow.style.display = 'none';
+  }
+}
+
+function getYear() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const displayYear = document.querySelector('#displayYear');
+  displayYear.innerHTML = currentYear;
+}
+getYear();
